16a1b85e3c4e228ff10a5bee960415c8e96a8c86
DESKTOP-89LA26K\vidma <vidmantaszuk@gmail.com>
2023-11-05 17:27:02 +0200

    System pager with controler and main style edit

---
+++ Faxai/Faxai/Controllers/PageSystemController.cs
@@ -0,0 +1,35 @@
+﻿using Faxai.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Faxai.Controllers
+{
+    public class PageSystemController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public PageSystemController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult PageConfig()
+        {
+            return View();
+        }
+        public IActionResult PageAnalitics()
+        {
+            return View();
+        }
+        public IActionResult EmailTemplates()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
