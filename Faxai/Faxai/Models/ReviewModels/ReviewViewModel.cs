2cc1f1d0893a2bcf4ccaf580d9ee20f8d3eb2192
Ignaseismantas <ignas.eismantas@gmail.com>
2023-12-17 23:59:56 +0200

    Fixed Review Creation bug with new Product Categories

--- Faxai/Faxai/Models/ReviewModels/ReviewViewModel.cs
+++ Faxai/Faxai/Models/ReviewModels/ReviewViewModel.cs
@@ -5,7 +5,7 @@ namespace Faxai.Models.ReviewModels
     public class ReviewViewModel
     {
         public ProductViewModel Product { get; set; }
-        public List<ReviewModel> Reviews { get; set; }
+        public List<ReviewModel>? Reviews { get; set; }
         public ReviewModel NewReview { get; set; }
 
     }
