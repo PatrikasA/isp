6dc46a2d507e14dd838faad565ff943a1149ff28
Gytis Užkuraitis <gytis.uzkuraitis@gmail.com>
2023-12-17 16:37:12 +0200

    Added ability to add new product to database

---
+++ Faxai/Faxai/Models/ProductModels/ProductCreateModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Faxai.Models.ProductModels
+{
+    public class ProductCreateModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Pavadinimas yra privalomas")]
+        public string Pavadinimas { get; set; }
+
+        [Required(ErrorMessage = "Kaina yra privaloma")]
+        public decimal Kaina { get; set; }
+
+        [Display(Name = "Kiekis Sandelyje")]
+        public int Kiekis_Sandelyje { get; set; }
+
+        public string Aprasymas { get; set; }
+
+        [Display(Name = "Zemiausia Kaina Per 10d")]
+        public decimal Zemiausia_Kaina_Per_10d { get; set; }
+    }
+
+}
+
